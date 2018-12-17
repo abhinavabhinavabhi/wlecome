84c952bd247cdf05a5a7ac6cf7bccbfa29c7bacd
abhinav <abhinavsujina@gmail.com>
2018-12-17 22:19:54 +0530

    Add project files.

---
+++ wlecome/login.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace wlecome
+{
+    public partial class login : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\project\wlecome\wlecome\atmdatabase.mdf;Integrated Security=True");
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'atmdatabaseDataSet.customer' table. You can move, or remove it, as needed.
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter d;
+             DataTable dt;
+           con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from customer where cust_id='" + textBox1.Text + "'and password='" + textBox2.Text + "'";
+            try
+            {
+                cmd.ExecuteNonQuery();
+
+                d = new SqlDataAdapter(cmd.CommandText, con);
+                dt = new DataTable();
+                d.Fill(dt);
+                con.Close();
+
+
+                if (dt.Rows.Count == 1)
+                {
+                    MessageBox.Show("passowrd mached");
+                    this.Close();
+                    dashbord ds = new dashbord();
+                    ds.ShowDialog();
+
+                }
+                else
+                {
+                    MessageBox.Show("incorrect password");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("check input");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+
+       
+    }
+}
---
+++ wlecome/welcome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+namespace wlecome
+{
+    public partial class welcome : Form
+    {
+        System.Timers.Timer tim = new System.Timers.Timer(3000);
+        
+        public welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void welcome_Load(object sender, EventArgs e)
+        {
+            tim = new System.Timers.Timer(3000);
+            tim.Elapsed += Tim_Elapsed;
+            tim.AutoReset = false;
+            tim.Enabled = true;
+            
+           
+        }
+
+        private  void Tim_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Form.CheckForIllegalCrossThreadCalls = false;
+            
+            login l = new login();
+            
+            this.Hide();
+            l.ShowDialog();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
